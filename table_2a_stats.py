e62a40415a5009674d20b714d74cd87b204e7a4a
Colin Stern <cstern19@gmail.com>
2017-02-09 02:39:44 -0500

    Created submission folders. Moved HTML files for each chart into different folders. Created bar charts for chart_2a and chart_2j, created a scatterplot with line of regression for 2a and 2j combined.

---
+++ table_2a_stats.py
@@ -0,0 +1,29 @@
+import csv
+import numpy as np
+
+# Import census data from CSV
+with open('table2a_edu_income _python.csv', 'r') as f:
+	my_reader = csv.reader(f)
+	data = list(my_reader)
+
+# Find mean
+
+mean = 0
+
+for i in range(0, len(data)):
+	mean = mean + int(data[i][1])
+
+mean = mean / (len(data))
+
+print ('Mean: ' + repr(mean))
+
+# Find standard deviation
+
+stdData = []
+
+for i in range (0, len(data)):
+	stdData.append(data[i][1])
+
+stdData = list(map(int, stdData))
+stdDev = round(np.std(stdData), 3)
+print ('Standard deviation: ' + repr(stdDev))
